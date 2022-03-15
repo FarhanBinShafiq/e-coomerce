88e83e7137ce64f4e963c1ec09f1bb65f67fda6a
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2022-03-16 02:28:35 +0600

    body Part

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
  
 import './App.css';
+import HomeTopBody from './pages/Home/HomeTopBody/HomeTopBody';
 import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <HomeTopBody></HomeTopBody>
     </>
   );
 }
